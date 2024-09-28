ac7797dd7b45f6c3a2c2f2546235d46a65489a37
Jihyeok Park <jihyeok_park@korea.ac.kr>
2024-09-28 13:48:01 +0900

    Updated

---
+++ src/main/g8/src/main/scala/kuplrg/Implementation.scala
@@ -0,0 +1,11 @@
+package kuplrg
+
+object Implementation extends Template {
+
+  import Expr.*
+  import Value.*
+
+  def interp(expr: Expr, env: Env, mem: Mem): (Value, Mem) = ???
+
+  def interpCBR(expr: Expr, env: Env, mem: Mem): (Value, Mem) = ???
+}
